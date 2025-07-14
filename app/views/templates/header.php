80107f87ecdb87afe3220f4e08f92f1470b789c4
intan0745 <i18111801@gmail.com>
2025-07-15 00:33:10 +0700

    tambahkan folder public/assets dan include CSS/JS

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,5 +3,6 @@
     <head>
         <meta charset="UTF-8">
         <title>Halaman <?= $data['judul']; ?></title>
-</head>
+	<link rel="stylesheet" href="http://localhost/Uasweb1_Sriintan/public/css/bootstrap.css">
+    </head>
 <body>
