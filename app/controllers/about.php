5a2feb0a53ffe5aeb3f5122bba7ab0ef22d5de9e
intan0745 <i18111801@gmail.com>
2025-07-14 22:49:11 +0700

    implementasikan Home Controller

---
+++ app/controllers/about.php
@@ -0,0 +1,15 @@
+<?php
+
+class About {
+    public function index($nama = 'sriintan', $pekerjaan = 'Dosen')
+    {
+
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+     public function page()
+        {
+            echo 'About/page';
+
+        }
+     
+}
