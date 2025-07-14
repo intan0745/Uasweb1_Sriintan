776b74aeca3563910e2765ee108e35e270afc5c2
intan0745 <i18111801@gmail.com>
2025-07-14 23:25:40 +0700

    tambahkan template layout dasar

--- app/controllers/about.php
+++ app/controllers/about.php
@@ -1,10 +1,11 @@
 <?php
 
 class About {
-    public function index($nama = 'sriintan', $pekerjaan = 'Dosen')
+    public function index($nama = '
+sriintan', $pekerjaan = 'Dosen', $umur = 32)
     {
 
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan. saya berumur $umur tahun.";
     }
      public function page()
         {
