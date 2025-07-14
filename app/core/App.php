4bdd975c9e3971e20aecb3bb4ff7ef66036418f2
intan0745 <i18111801@gmail.com>
2025-07-14 21:43:29 +0700

    membuat sistem ROUTING untuk mengelola url, dan buat kelas base controller

--- app/core/App.php
+++ app/core/App.php
@@ -2,9 +2,24 @@
 
 class App {
     public function __construct()
-        {
-            echo 'Ok!';
-        }
-    
+    {
+        $url = $this->parseURL();
 
+        // Tambahkan <pre> agar output var_dump rapi
+        echo '<pre>';
+        var_dump($url);
+        echo '</pre>';
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+
+        return []; // return array kosong jika tidak ada URL
+    }
 }
