03bcd063c3354686220fc578706cc84946d2f161
intan0745 <i18111801@gmail.com>
2025-07-15 07:58:45 +0700

    buat Itemcontroller

--- app/models/mahasiswa_model.php
+++ app/models/mahasiswa_model.php
@@ -3,5 +3,22 @@
 class Mahasiswa_model {
     private $dbh;  // database handler
     private $stmt; // statement handler
+  public function __construct()
+    {
+        // data source name
+        $dsn = 'mysql:host=localhost;dbname=uasweb1_sriintan';
+        
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
+    public function getAllMahasiswa()
+    {
+        $this->stmt = $this->dbh->prepare("SELECT * FROM mahasiswa");
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
